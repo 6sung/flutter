8a98831deb71b12396259b686f61170f34fa12e3
6sung <fredangel1219@gmail.com>
2024-03-19 01:22:19 +0900

    first push:

---
+++ flutter/rsp_game/lib/game/widget/game_result.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../enum.dart';
+
+class GameResult extends StatelessWidget {
+  final bool isDone;
+  final Result? result;
+  final VoidCallback callback;
+
+  const GameResult({required this.isDone,required this.callback, this.result,super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if(isDone){
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text(result!.displayString, style: TextStyle(fontSize: 32),),
+          ),
+          const SizedBox(height: 8,),
+          TextButton(
+            child: const Text('다시 하기', style: const TextStyle(fontSize: 24),),
+            onPressed: () => callback.call(),
+          )
+        ],
+      );
+    }
+    return const Center(
+      child: Text('가위 바위 보 중에 하나를 선택 해 주세요.', style: TextStyle(fontSize: 24,fontWeight:FontWeight.bold))
+    );
+  }
+}
